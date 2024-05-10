18825c7d1e257c8d3f2a79d1a9dd69cbafbd4a6f
Arthur Carneiro Lobo <arthurlbo16@gmail.com>
2024-05-10 00:04:57 -0300

    chore: removing unnecessary configuration and dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,28 +4,7 @@ module.exports = {
     content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
     prefix: "",
     theme: {
-        container: {
-            center: true,
-            padding: "2rem",
-            screens: {
-                "2xl": "1400px",
-            },
-        },
         extend: {
-            keyframes: {
-                "accordion-down": {
-                    from: { height: "0" },
-                    to: { height: "var(--radix-accordion-content-height)" },
-                },
-                "accordion-up": {
-                    from: { height: "var(--radix-accordion-content-height)" },
-                    to: { height: "0" },
-                },
-            },
-            animation: {
-                "accordion-down": "accordion-down 0.2s ease-out",
-                "accordion-up": "accordion-up 0.2s ease-out",
-            },
             colors: {
                 background: "#000000",
                 primary: "#F2F2F2",
@@ -43,5 +22,5 @@ module.exports = {
             },
         },
     },
-    plugins: [require("tailwindcss-animate"), require("tailwind-scrollbar")({ nocompatible: true })],
+    plugins: [require("tailwind-scrollbar")({ nocompatible: true })],
 };
